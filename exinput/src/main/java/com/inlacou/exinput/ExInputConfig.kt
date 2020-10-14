1864395d18afa01d42ffe54a5e34d1351d8578f4
Inlacou <inlacou@gmail.com>
2020-10-14 09:52:35 +0200

    minor UI fix

--- exinput/src/main/java/com/inlacou/exinput/ExInputConfig.kt
+++ exinput/src/main/java/com/inlacou/exinput/ExInputConfig.kt
@@ -4,8 +4,16 @@ import java.util.*
 
 object ExInputConfig {
 
-	val toDate: (Calendar) -> String = { "${it.get(Calendar.DAY_OF_MONTH)}/${it.get(Calendar.MONTH)}/${it.get(Calendar.YEAR)}" }
-	val toTime: (Calendar) -> String = { "${it.get(Calendar.HOUR_OF_DAY)}:${it.get(Calendar.MINUTE)}" }
+	val toDate: (Calendar) -> String = { "${it.get(Calendar.DAY_OF_MONTH).toStringMinDigits()}/${it.get(Calendar.MONTH).toStringMinDigits()}/${it.get(Calendar.YEAR)}" }
+	val toTime: (Calendar) -> String = { "${it.get(Calendar.HOUR_OF_DAY).toStringMinDigits()}:${it.get(Calendar.MINUTE).toStringMinDigits()}" }
 	val toDateTime: (Calendar) -> String = { "${toDate.invoke(it)} ${toTime.invoke(it)}" }
 
+}
+
+private fun Int.toStringMinDigits(minDigits: Int = 2): String {
+	var result = this.toString()
+	while (result.length<minDigits) {
+		result = "0$result"
+	}
+	return result
 }
