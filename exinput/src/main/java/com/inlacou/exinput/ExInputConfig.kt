f2a458624781ba8a7ec0c5c46b0a001cc0a1633d
Inlacou <inlacou@gmail.com>
2020-10-14 10:06:55 +0200

    another UI fix

--- exinput/src/main/java/com/inlacou/exinput/ExInputConfig.kt
+++ exinput/src/main/java/com/inlacou/exinput/ExInputConfig.kt
@@ -4,7 +4,7 @@ import java.util.*
 
 object ExInputConfig {
 
-	val toDate: (Calendar) -> String = { "${it.get(Calendar.DAY_OF_MONTH).toStringMinDigits()}/${it.get(Calendar.MONTH).toStringMinDigits()}/${it.get(Calendar.YEAR)}" }
+	val toDate: (Calendar) -> String = { "${it.get(Calendar.DAY_OF_MONTH).toStringMinDigits()}/${(it.get(Calendar.MONTH)+1).toStringMinDigits()}/${it.get(Calendar.YEAR)}" }
 	val toTime: (Calendar) -> String = { "${it.get(Calendar.HOUR_OF_DAY).toStringMinDigits()}:${it.get(Calendar.MINUTE).toStringMinDigits()}" }
 	val toDateTime: (Calendar) -> String = { "${toDate.invoke(it)} ${toTime.invoke(it)}" }
 
